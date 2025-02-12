0a0342ab157a8ee26210c235f3b166e44604e180
PaulBaFi <paulalexisbarahonafigueroa@gmail.com>
2025-02-12 18:39:47 -0500

    feat: Creación de la  HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -23,11 +23,54 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
-      children: [
-        Text('Hello World'),
-      ],
-    ));
+    return Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.yellow, Colors.deepOrange, Colors.deepOrange]),
+        ),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Stack(
+              children: [
+                Positioned(
+                    top: 150,
+                    left: 0,
+                    right: 0,
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage('lib/assets/images/logo.png'),
+                          width: 250,
+                        ),
+                        SizedBox(height: 100),
+                        homeButton('ALUMNOS'),
+                        SizedBox(height: 10),
+                        homeButton('INSTRUCTORES'),
+                        SizedBox(height: 10),
+                        homeButton('VECHÍCULOS'),
+                        SizedBox(height: 10),
+                        homeButton('REPORTES'),
+                      ],
+                    )),
+              ],
+            )));
+  }
+
+  OutlinedButton homeButton(String title) {
+    return OutlinedButton(
+        onPressed: () {},
+        style: OutlinedButton.styleFrom(
+            minimumSize: Size(350, 60),
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.transparent,
+            side: BorderSide(color: Colors.white),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0))),
+        child: Text(
+          title,
+          style: TextStyle(letterSpacing: 2, fontSize: 20),
+        ));
   }
 }
